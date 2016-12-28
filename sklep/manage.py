f1017ee75a952dbd58c59b35bd40ca6778a5d586
Fytyny <marcin123332@hotmail.com>
2016-12-29 00:25:33 +0100

    register page

--- sklep/manage.py
+++ sklep/manage.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-<<<<<<< HEAD
-=======
 
->>>>>>> origin
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sklep.settings")
     try:
--- sklep/sklep/urls.py
+++ sklep/sklep/urls.py
@@ -13,18 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-<<<<<<< HEAD
 from django.conf.urls import url,include
 from django.contrib import admin
 from account.views import register
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register', register, name = 'register')
-=======
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
->>>>>>> origin
 ]
